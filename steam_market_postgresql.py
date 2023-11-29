e7a7fd1179255cece90aecc4f9f1a5883287772a
Robert <rf.filusz@gmail.com>
2023-11-29 19:15:37 +0100

    A version that stores data in a database instead of Excel file

--- steam_market_postgresql.py
+++ steam_market_postgresql.py
@@ -11,33 +11,46 @@ try:
         password="p4$S(postgres)W0r|)",
         host="127.0.0.1",
         port=5432,
-        database="investment2"
-
+        database="investment",
     )
 except psycopg2.Error as e:
     print(f"Error connecting to PostgreSQL Platform: {e}")
     sys.exit(1)
-cur=conn.cursor()
-cur.execute('DROP TABLE IF EXISTS investments')
-cur.execute('CREATE TABLE investments (name TEXT, price TEXT)')
+cur = conn.cursor()
+cur.execute("DROP DATABASE IF EXISTS investment")
+cur.execute("CREATE DATABASE investment")
+cur.execute("DROP TABLE IF EXISTS investments")
+cur.execute("CREATE TABLE investments (name TEXT, price TEXT)")
 
-response=requests.get("https://steamcommunity.com/market/search?q=paris+2023+capsule")
-soup=BeautifulSoup(response.text,'lxml')
+response = requests.get("https://steamcommunity.com/market/search?q=paris+2023+capsule")
+soup = BeautifulSoup(response.text, "lxml")
 
 
-items = soup.find_all('a', class_='market_listing_row_link')
+items = soup.find_all("a", class_="market_listing_row_link")
 for element in items:
-    #names=soup.find(class_="market_listing_item_name") ||| for some reason prints the same item multiple times instead of multiple items once
-    pieces=element.text.split()
-    name=pieces[7]+" "+pieces[8]+" "+pieces[9]+" "+pieces[10]+" "+pieces[11]
-    price=pieces[3]
-    cur.execute('SELECT price FROM investments WHERE name=%s',(name,))
-    row=cur.fetchone()
+    # names=soup.find(class_="market_listing_item_name") ||| for some reason prints the same item multiple times instead of multiple items once
+    pieces = element.text.split()
+    name = (
+        pieces[7]
+        + " "
+        + pieces[8]
+        + " "
+        + pieces[9]
+        + " "
+        + pieces[10]
+        + " "
+        + pieces[11]
+    )
+    price = pieces[3]
+    cur.execute("SELECT price FROM investments WHERE name=%s", (name,))
+    row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO investments (name, price) VALUES (%s, %s)''',(name,price))
+        cur.execute(
+            """INSERT INTO investments (name, price) VALUES (%s, %s)""", (name, price)
+        )
     conn.commit()
 
-cur.execute('SELECT name, price FROM Investments ORDER BY price DESC LIMIT 10')
+cur.execute("SELECT name, price FROM Investments ORDER BY price DESC LIMIT 10")
 for row in cur.fetchall():
     print(row)
 
